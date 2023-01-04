b4faec5511f7d591c6ca2a9af2d0ce4f2aff911a
hale-bopp97 <mattastik@gmail.com>
2023-01-04 00:09:34 -0800

    separated todo and work lists, and added an about page

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-var item = ''
-var items = ['buy food', 'cook food', 'eat food']
+var item      = ''
+var items     = ['buy food', 'cook food', 'eat food']
+var workItems = []
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -22,17 +23,46 @@ app.get('/', function(req, res) {
 
     var day = today.toLocaleDateString('en-US', options)
 
-    res.render('list', {kindOfDay: day, newListItems: items})
+    res.render('list', {listTitle: day, newListItems: items})
 
 })
 
 app.post('/', function(req, res) {
 
+    console.log(req.body)
     item = req.body.newItem
+
+    if (req.body.list === 'Work') {
+        
+        workItems.push(item)
+        res.redirect('/work')
+    
+    } else {
+
+        items.push(item)
+        res.redirect('/')
+
+    }
+
+})
+
+app.get('/work', function(req, res) {
+    res.render('list', {listTitle: 'Work List', newListItems: workItems})
+})
+
+app.post('/work', function(req, res) {
+    
+    let item = req.body.newItem
+
     items.push(item)
-    // console.log(item)
+    console.log(req.body)
+
     res.redirect('/')
 
+})
+
+app.get('/about', function(req, res) {
+    res.render('about')
 })
 
 app.listen(3000, function() {
